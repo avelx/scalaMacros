527c53613f8501e1825b811bf4e3d01581cfaeb1
Pavel <pavel.oliynyk@gmail.com>
2025-05-27 13:05:02 +0100

    more complex examples;

--- src/main/scala/Macros.scala
+++ src/main/scala/Macros.scala
@@ -2,10 +2,15 @@ object Macros {
   // Simple algebraic data type
 
   sealed trait Exp
+
   case class Num(value: Int) extends Exp
+
   case class Add(left: Exp, right: Exp) extends Exp
+
   case class Mul(left: Exp, right: Exp) extends Exp
+
   case class Sub(left: Exp, right: Exp) extends Exp
+
   case class Div(left: Exp, right: Exp) extends Exp
 
 
@@ -21,12 +26,44 @@ object Macros {
 
   inline def secondMacro(number: Int, string: String): String =
     ${ secondMacroImp('number, 'string) }
-  
-  def secondMacroImp(num: Expr[Int], strExpr: Expr[String]) (using Quotes): Expr[String] = {
+
+  def secondMacroImp(num: Expr[Int], strExpr: Expr[String])(using Quotes): Expr[String] = {
     val theNumber: Int = num.valueOrAbort
-    if theNumber > 10 then 
-      Expr(s"This is the number: $theNumber")
-    else 
-      throw new Error(s"Unxpected number: $theNumber")  
+    val theString: String = strExpr.valueOrAbort
+    if theNumber < 10 then {
+      val res = Expr(s"This is the number: ${theString.repeat(theNumber)}")
+      println(s"Executed at RT: ${res.valueOrAbort}")
+      res
+    }
+    else {
+      throw new Error(s"Unxpected number: $theNumber")
+    }
+  }
+
+  inline def pmOption(inline opt: Option[Int]): String = ${ pmOptionImp('opt) }
+
+  def pmOptionImp(opt: Expr[Option[Int]])(using Quotes): Expr[String] = {
+    val res = opt match {
+      case '{ Some(42) } => Expr("I found the meaning of life")
+      case '{ Some($x) } => Expr(s"got a variable: ${x.show}")
+      case _ => Expr("Got something else")
+    }
+    println(res.valueOrAbort)
+    res
+  }
+
+  inline def optimized[A](inline list: List[A]): String =
+    ${ optimizedImpl[A]('list) }
+
+  def optimizedImpl[A](list: Expr[List[A]])(using Quotes): Expr[String] = {
+    val desc = list match {
+      case '{
+//            type t1
+            ($original: List[t1]).map[t2]($f).map[t3]($g)
+        } => s"Some result: ${f.show} and ${g.show}"
+      case l
+        => "simple list"
+    }
+    Expr(desc)
   }
 }
--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -1,7 +1,16 @@
 object Runner extends App {
   import Macros._
 
+  //def getValue: Int = 1
+
   // Prevent compilation if conditons not met
-  private val macroValue = secondMacro(11, "data")
+  private val macroValue = secondMacro( 3, "data")
   println(macroValue)
+
+  val pmOptions = pmOption(Some(42))
+  println(pmOptions)
+
+  val optRes = optimized( List(1, 4, 5, 8, 9).map(x => x * 2).map(y => y - 1) )
+  println(optRes)
+
 }
